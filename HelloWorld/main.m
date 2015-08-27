fdfc2b3b38ec7f30082e2d8a0a22b58276f6dfa4
James Hofstra <jameshofstra@users.noreply.github.com>
2015-08-27 01:33:03 -0700

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by James Hofstra on 8/27/15.
+//  Copyright (c) 2015 James Hofstra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
